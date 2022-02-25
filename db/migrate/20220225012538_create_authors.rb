e4ad8eee432fe7ee5f21eec736bdfdbc4c111b7e
Fabiane Albuquerque <bii.nett@gmail.com>
2022-02-24 22:31:45 -0300

    [Refactor]: Change db to postgresql

---
+++ db/migrate/20220225012538_create_authors.rb
@@ -0,0 +1,12 @@
+class CreateAuthors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :authors, id: :uuid do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :birth_year
+      t.boolean :alive
+
+      t.timestamps
+    end
+  end
+end
