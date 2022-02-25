60005e3872b4d06aad3d5e781b162abbdf28d9b8
Fabiane Albuquerque <bii.nett@gmail.com>
2022-02-24 23:48:38 -0300

    [Feature]: Add create Author
    example with variables:
    
    ```
    mutation createAuthor($name: String, $last: String, $b_year: Int,
        $alive: Boolean){
              createAuthor(
                    firstName: $name
                    lastName: $last
                    birthYear: $b_year
                    alive: $alive){
    
                    id
                    firstName
                    fullName
            }
    
    }
    
    ---- variables ----
    
    {
      "name": "Xita",
      "last": "rps",
      "b_year": 1990,
      "alive": true
    
    }
    
    ```

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,22 @@
+# criar, alterar ou apagar algo (C_UD)
 module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World"
+    # end
+                          # model
+    field :create_author, Types::AuthorType, null: false, description: "Create an author" do
+      argument :first_name,  String,  required: false
+      argument :last_name,   String,  required: false
+      argument :birth_year,  Int,     required: false
+      argument :alive,       Boolean, required: false
+    end
+    def create_author(first_name:, last_name:, birth_year:, alive:)
+      Author.create(first_name: first_name, last_name: last_name,
+                    birth_year: birth_year, alive: alive)
     end
   end
 end
