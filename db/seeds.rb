584f387315ae7f74d56249f06a60c3ede14bd422
Fabiane Albuquerque <bii.nett@gmail.com>
2022-02-24 22:39:50 -0300

    [Feature]: Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+authors = [
+  {first_name: "Alice", last_name: "Santos", birth_year: 1990, alive: true},
+  {first_name: "Breno", last_name: "Pereira", birth_year: 2010, alive: true},
+  {first_name: "Clovis", last_name: "Aguiar", birth_year: 1980, alive: false},
+]
+
+Author.create(authors)
